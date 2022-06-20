9d4b01573741ef09d28017c694d66a29d1059092
Gwanho <>
2022-06-20 14:23:01 +0900

    ff

--- src/main/java/org/springframework/samples/petclinic/model/Vet.java
+++ src/main/java/org/springframework/samples/petclinic/model/Vet.java
@@ -27,7 +27,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.xml.bind.annotation.XmlElement;
+
 
 import org.springframework.beans.support.MutableSortDefinition;
 import org.springframework.beans.support.PropertyComparator;
@@ -60,7 +60,7 @@ public class Vet extends Person {
         this.specialties = specialties;
     }
 
-    @XmlElement
+
     public List<Specialty> getSpecialties() {
         List<Specialty> sortedSpecs = new ArrayList<>(getSpecialtiesInternal());
         PropertyComparator.sort(sortedSpecs, new MutableSortDefinition("name", true, true));
