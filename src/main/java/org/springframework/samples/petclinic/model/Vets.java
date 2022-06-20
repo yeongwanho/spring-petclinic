49b17af372f37e877d4ce73c617d4c54fcd00c2e
Gwanho <>
2022-06-20 14:50:49 +0900

    ff

--- src/main/java/org/springframework/samples/petclinic/model/Vets.java
+++ src/main/java/org/springframework/samples/petclinic/model/Vets.java
@@ -18,8 +18,7 @@ package org.springframework.samples.petclinic.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
+
 
 /**
  * Simple domain object representing a list of veterinarians. Mostly here to be used for the 'vets' {@link
@@ -27,12 +26,12 @@ import javax.xml.bind.annotation.XmlRootElement;
  *
  * @author Arjen Poutsma
  */
-@XmlRootElement
+
 public class Vets {
 
     private List<Vet> vets;
 
-    @XmlElement
+
     public List<Vet> getVetList() {
         if (vets == null) {
             vets = new ArrayList<>();
